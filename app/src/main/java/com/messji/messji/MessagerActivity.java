9848417c520c2d55b6bafe0e899b3513731907fe
peterfoxflick <peterfoxflick@gmail.com>
2018-10-23 19:40:41 -0600

    Intial Build
    The page is mostly built, some issues with emojis and user avitar

--- app/src/main/java/com/messji/messji/MessagerActivity.java
+++ app/src/main/java/com/messji/messji/MessagerActivity.java
@@ -1,7 +1,9 @@
 package com.messji.messji;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MessagerActivity extends AppCompatActivity {
 
@@ -10,4 +12,13 @@ public class MessagerActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_messager);
     }
+
+    public void clickedProfile(View view) {
+
+
+        Intent intent = new Intent(this, ProfileActivity.class);
+
+        startActivity(intent);
+    }
+
 }

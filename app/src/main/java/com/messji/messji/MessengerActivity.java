15e5049092d1e8306079f27552742593575d71e7
peterfoxflick <peterfoxflick@gmail.com>
2018-11-14 14:48:32 -0700

    Added draft ability to new texts

--- app/src/main/java/com/messji/messji/MessengerActivity.java
+++ app/src/main/java/com/messji/messji/MessengerActivity.java
@@ -1,5 +1,10 @@
 package com.messji.messji;
 
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -18,122 +23,52 @@ import com.scaledrone.lib.Scaledrone;
 import java.util.Random;
 
 public class MessengerActivity extends AppCompatActivity {
-    ;
     private EditText editText;
     // private Scaledrone scaledrone;
     private MessageAdapter messageAdapter;
     private ListView messagesView;
+    private int convId;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+        Intent intent = getIntent();
+        convId = intent.getIntExtra("convId", -1);
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_messenger);
         editText = (EditText) findViewById(R.id.editText);
 
+        // if negative 1 go back to home
+
+        //Use conv ID to populate messages here
+
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
+
+        if(prefs.getInt("lastConvId", -1) == convId){
+            String draft = prefs.getString("draftText", "");
+            editText.setText(draft);
+        }
+
         messageAdapter = new MessageAdapter(this);
         messagesView = (ListView) findViewById(R.id.messages_view);
         messagesView.setAdapter(messageAdapter);
+    }
 
-       /*     @Override
-            public void onOpen() {
-                System.out.println("IN ONOPEN METHOD");
-                // Since the MainActivity itself already implement RoomListener we can pass it as a target
-                scaledrone.subscribe(roomName, MessengerActivity.this);
-            }
+    public void onClose(){
 
-            @Override
-            public void onOpenFailure(Exception ex) {
-                System.err.println(ex);
-            }
+        SharedPreferences sharedPref = this.getPreferences(Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPref.edit();
+        editor.putString("draftText",editText.getText().toString());
+        editor.putInt("convId", convId );
+        editor.commit();
+    }
 
-            @Override
-            public void onFailure(Exception ex) {
-                System.err.println(ex);
-            }
 
-            @Override
-            public void onClosed(String reason) {
-                System.err.println(reason);
-            }*/
-        };
-//
-//    @Override
-//    public void onOpen(Room room) {
-//        System.out.println("Conneted to room");
-//    }
-//
-//    @Override
-//    public void onOpenFailure(Room room, Exception ex) {
-//        System.err.println(ex);
-//    }
-//
-//
-    //@Override
-    /*public void onMessage(Room room, final JsonNode json, final Member member) {
-        System.out.print("IN ONMESSAGE METHOD@@@@@@@@@@@@@@@@@@@@@@");
-        // To transform the raw JsonNode into a POJO we can use an ObjectMapper
-        final ObjectMapper mapper = new ObjectMapper();
-        try {
-            // member.clientData is a MemberData object, let's parse it as such
-            final MemberData data = mapper.treeToValue(member.getClientData(), MemberData.class);
-            // if the clientID of the message sender is the same as our's it was sent by us
-            boolean belongsToCurrentUser = member.getId().equals(scaledrone.getClientID());
-            // since the message body is a simple string in our case we can use json.asText() to parse it as such
-            // if it was instead an object we could use a similar pattern to data parsing
-            final Message message = new Message(json.asText(), data, belongsToCurrentUser);
-            runOnUiThread(new Runnable() {
-                @Override
-                public void run() {
-                    messageAdapter.add(message);
-                    // scroll the ListView to the last added element
-                    messagesView.setSelection(messagesView.getCount() - 1);
-                }
-            });
-        } catch (JsonProcessingException e) {
-            e.printStackTrace();
-        }
-    }*/
+
 
     public void sendMessage(View view) {
         System.out.println("IN SENDMESSAGE FUNCTION");
-        /*String message = editText.getText().toString();
-        if (message.length() > 0) {
-            scaledrone.publish("observable-room", message);
-            editText.getText().clear();*/
+
     }
 }
-
-//   }
-//
-//
-//class MemberData {
-//    private String name;
-//    private String color;
-//
-//    public MemberData(String name, String color) {
-//        this.name = name;
-//        this.color = color;
-//    }
-//
-//    // Add an empty constructor so we can later parse JSON into MemberData using Jackson
-//    public MemberData() {
-//    }
-//
-//    public String getName() {
-//        return name;
-//    }
-//
-//    public String getColor() {
-//        return color;
-//    }
-//
-//    @Override
-//    public String toString() {
-//        return "MemberData{" +
-//                "name='" + name + '\'' +
-//                ", color='" + color + '\'' +
-//                '}';
-//    }
-//}
-
-

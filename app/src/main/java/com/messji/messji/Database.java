dca12d9afa2e5384c30ef80a0cd046dfef1a7212
peterfoxflick <peterfoxflick@gmail.com>
2018-11-28 14:55:27 -0700

    Added JavaDocs

--- app/src/main/java/com/messji/messji/Database.java
+++ app/src/main/java/com/messji/messji/Database.java
@@ -22,23 +22,43 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+/**
+ * Acts as a database for the app storing a list of messages, conversations, and users.
+ * The information is initially pulled from a starting file, and then stored as a
+ * shared preference.
+ */
 public final class Database {
     private static List messages;
     private static List conversations;
     private static List users;
 
+    /**
+     * Returns a list of all messages
+     */
     public static List getMessages() {
         return messages;
     }
 
+    /**
+     * Returns a list of all conversations
+     */
     public static List getConversations() {
         return conversations;
     }
 
+    /**
+     * Returns the list of all users (contacts lists)
+     * @return List of Users
+     */
     public static List getUsers() {
         return users;
     }
 
+    /**
+     * Pulls the users from the users.json file. This acts as an inital list of users for the
+     * app
+     * @param context Nesccessary to find the file. From an activity just pass in "this"
+     */
     public static void loadUsers(Context context) {
         Gson gson = new Gson();
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(context.getResources().openRawResource(R.raw.contacts)));
@@ -46,6 +66,11 @@ public final class Database {
         Log.i("Users: ", users.toString());
     }
 
+    /**
+     * Pulls the messages from the messages.json file. This will act as an intial set of messages for
+     * the app.
+     * @param context Nesccessary to find the file. From an activity just pass in "this"
+     */
     public static void loadMessages(Context context) {
         Gson gson = new Gson();
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(context.getResources().openRawResource(R.raw.messages)));
@@ -53,6 +78,11 @@ public final class Database {
         Log.i("Messages: ", messages.toString());
     }
 
+    /**
+     * Pulls the conversations from the conversation.json file. This will act as an intial set of conversations for
+     * the app.
+     * @param context Nesccessary to find the file. From an activity just pass in "this"
+     */
     public static void loadConversations(Context context) {
         Gson gson = new Gson();
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(context.getResources().openRawResource(R.raw.conversations)));
@@ -60,7 +90,10 @@ public final class Database {
         Log.i("Conversations: ", conversations.toString());
     }
 
-    public void save(Context context) {
+    /**
+     * Saves the new information added to the database so it can be used next time the app is launched
+     */
+    public void save() {
         String mes = new Gson().toJson(messages);
         try {
             Writer output = new BufferedWriter(new FileWriter("messages.json"));
@@ -83,6 +116,11 @@ public final class Database {
     }
 
     // Class to write messages back into the Database class?
+
+    /**
+     * Adds a message to the database so it can be stored for future use.
+     * @param message The message to save
+     */
     public static void addMessage(Message message) {
         try {
             Writer output = new BufferedWriter(new FileWriter("messages.json"));

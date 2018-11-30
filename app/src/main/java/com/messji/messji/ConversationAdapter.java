c35203388be98dec645a73adeb81a67527aba185
Henrique Tedeschi <tedeschi@byui.edu>
2018-11-29 19:47:10 -0700

    When clicking on a conversation, opens the Messenger activity passing the conversation data

--- app/src/main/java/com/messji/messji/ConversationAdapter.java
+++ app/src/main/java/com/messji/messji/ConversationAdapter.java
@@ -1,5 +1,6 @@
 package com.messji.messji;
 
+import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
@@ -7,6 +8,8 @@ import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import com.google.gson.Gson;
 
 import java.util.List;
 import java.util.Random;
@@ -32,7 +35,7 @@ public class ConversationAdapter extends RecyclerView.Adapter<ConversationViewHo
 
     // For when the view holder is bound to a new position
     @Override
-    public void onBindViewHolder(@NonNull ConversationViewHolder conversationViewHolder, int position) {
+    public void onBindViewHolder(@NonNull ConversationViewHolder conversationViewHolder, final int position) {
         Log.d("On Bind View", "Youre in");
 
         // Get the message at the position in the list and set the text to the view holder
@@ -43,6 +46,23 @@ public class ConversationAdapter extends RecyclerView.Adapter<ConversationViewHo
         conversationViewHolder.mLastMessage.setText(mConversation.get(position).title);
         conversationViewHolder.mTimeStamp.setText("4:53 PM 11/28/2018");
         conversationViewHolder.mAvatar.setImageResource(R.drawable.avitar);
+
+
+        conversationViewHolder.mBackground.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.i(v.getContext().toString(), "Getting ready to open an existent conversation. Index: " + position);
+
+                Intent intent = new Intent(v.getContext(), MessengerActivity.class);
+
+                Conversation conversation = mConversation.get(position);
+                intent.putExtra("Conversation", new Gson().toJson(conversation, Conversation.class));
+
+                v.getContext().startActivity(intent);
+
+                Log.i(v.getContext().toString(), "Activity opened. Intent: " + intent.toString());
+            }
+        });
 
         Log.d("On Bind View", "YOu set the text to Howdy Partner");
     }

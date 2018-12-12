ca80cab4ec26a1381ba5fa0a9ccaa50e9c099990
Henrique Tedeschi <tedeschi@byui.edu>
2018-12-11 21:30:39 -0700

    Contact Unit tests done

---
+++ app/src/test/java/com/messji/messji/ContactPackage/ContactTests.java
@@ -0,0 +1,77 @@
+package com.messji.messji.ContactPackage;
+
+import com.messji.messji.R;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ContactTests {
+
+    String _firstName = "John";
+    String _lastName = "Smith";
+    String _phone = "8013477691";
+    int _picture = R.drawable.ic_users_1;
+
+    Contact c = new Contact(_firstName, _lastName, _phone, _picture);
+
+    @Test
+    public void getAndSetId() {
+        c.setId(99);
+        assertNotNull(c.getId());
+        assertEquals(99, c.getId());
+    }
+
+    @Test
+    public void getFirstName() {
+        assertEquals(_firstName, c.getFirstName());
+    }
+
+    @Test
+    public void setFirstName() {
+        String firstName = "Brady";
+        c.setFirstName(firstName);
+        assertEquals(firstName, c.getFirstName());
+    }
+
+    @Test
+    public void getLastName() {
+        assertEquals(_lastName, c.getLastName());
+    }
+
+    @Test
+    public void setLastName() {
+        String lastName = "Christian";
+        c.setLastName(lastName);
+        assertEquals(lastName, c.getLastName());
+    }
+
+    @Test
+    public void getPhoneNumber() {
+        assertEquals(_phone, c.getPhoneNumber());
+    }
+
+    @Test
+    public void setPhoneNumber() {
+        String phone = "9787900290";
+        c.setPhoneNumber(phone);
+        assertEquals(phone, c.getPhoneNumber());
+    }
+
+    @Test
+    public void getPhoto() {
+        assertEquals(_picture, c.getPhoto());
+    }
+
+    @Test
+    public void setPhoto() {
+        int photo = R.drawable.ic_users_2;
+        c.setPhoto(photo);
+        assertEquals(photo, c.getPhoto());
+    }
+
+    @Test
+    public void getFullName() {
+        assertEquals(_firstName + " " + _lastName, c.getFullName());
+    }
+}
--- app/src/test/java/com/messji/messji/SendMessageTest.java
+++ app/src/test/java/com/messji/messji/SendMessageTest.java
@@ -1,5 +1,7 @@
 package com.messji.messji;
 
+import com.messji.messji.ConversationPackage.Conversation;
+
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
@@ -9,9 +11,17 @@ public class SendMessageTest {
     public void sendMessageTest() {
         //Check that messageID matches convo ID
         // create message
+        Message m = null;
+        m.setMessage_id(10);
+
         //create convo
+        Conversation c = null;
+
         //send message to covno
+        c.addMessage(m.getMessage_id());
+
         //assert convo listID contains messadeID
-        assertEquals(true, true);
+//        assertEquals(true, true);
+        assertEquals(c.getMessages(), m.getMessage_id());
     }
 }

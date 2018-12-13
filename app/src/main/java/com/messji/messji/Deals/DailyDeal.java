0930e76b88cb03b5f2f4dd4aced8b5de6ffe1e0b
peterfoxflick <peterfoxflick@gmail.com>
2018-12-12 22:33:37 -0700

    Freaky Friday deal is done

--- app/src/main/java/com/messji/messji/Deals/DailyDeal.java
+++ app/src/main/java/com/messji/messji/Deals/DailyDeal.java
@@ -20,7 +20,9 @@ public class DailyDeal {
         Calendar calendar = Calendar.getInstance();
         int day = calendar.get(Calendar.DAY_OF_WEEK);
 
-        return new WednesdayDeal(message);
+        return new FridayDeal(message);
+
+        //return new WednesdayDeal(message);
 
 //        switch (day) {
 //            case Calendar.MONDAY:

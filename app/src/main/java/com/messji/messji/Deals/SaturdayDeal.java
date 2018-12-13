4a06b20fbd1ef527f9e57d78b829dcd644885242
Henrique Tedeschi <tedeschi@byui.edu>
2018-12-12 22:53:11 -0700

    Saturday Deal

---
+++ app/src/main/java/com/messji/messji/Deals/SaturdayDeal.java
@@ -0,0 +1,40 @@
+package com.messji.messji.Deals;
+
+import com.messji.messji.Message;
+
+public class SaturdayDeal extends DailyDeal {
+
+    protected SaturdayDeal(Message message) {
+        super(message);
+
+        setTitle("Stealing Saturdat");
+
+        setDescription("The crime is loose! Sending a special emoji will steal 10 characteres from the contact you send it to!");
+
+        // This is the emoji 💰
+        setImageEmoji("\uD83D\uDCB0");
+    }
+
+    @Override
+    public void incomingMessage() {
+        int bags = getCharCount(getImageEmoji());
+        if (bags > 0)
+        {
+            //Future Note: cap the emoji limit
+            subtractCharCount(10);
+        }
+
+    }
+
+    @Override
+    protected int getLength() {
+        int bags = getCharCount(getImageEmoji());
+        int points = 0;
+
+        if (bags > 0){
+            points = 10;
+        }
+
+        return getMessage().getText().length() - points;
+    }
+}
